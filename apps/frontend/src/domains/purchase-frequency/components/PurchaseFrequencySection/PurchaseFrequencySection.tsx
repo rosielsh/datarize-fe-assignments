b96b25e05b77dae7a227d7e8595eb9f5e5322d38
suhwa lee <tnghk9611@naver.com>
2025-12-23 17:56:40 +0900

    feat: 구매 빈도 데이터 조회 기능 구현

--- apps/frontend/src/domains/purchase-frequency/components/PurchaseFrequencySection/PurchaseFrequencySection.tsx
+++ apps/frontend/src/domains/purchase-frequency/components/PurchaseFrequencySection/PurchaseFrequencySection.tsx
@@ -4,6 +4,7 @@ import DateInput from '@/shared/components/DateInput/DateInput';
 import { useState } from 'react';
 import { formatDate } from '../../formatters/formatDate';
 import { formatRange } from '../../formatters/formatRange';
+import { usePurchaseFrequency } from '../../hooks';
 import * as S from './PurchaseFrequencySection.styled';
 
 const PurchaseFrequencySection = () => {
@@ -12,17 +13,10 @@ const PurchaseFrequencySection = () => {
   const [isOpenCalendar, setIsOpenCalendar] = useState(false);
   const [selectType, setSelectType] = useState<'start' | 'end' | null>(null);
 
-  const sampleData = [
-    { range: '0 - 20000', count: 10 },
-    { range: '20001 - 30000', count: 16 },
-    { range: '30001 - 40000', count: 44 },
-    { range: '40001 - 50000', count: 28 },
-    { range: '50001 - 60000', count: 11 },
-    { range: '60001 - 70000', count: 17 },
-    { range: '70001 - 80000', count: 0 },
-    { range: '80001 - 90000', count: 0 },
-    { range: '90001 - 100000', count: 13 },
-  ];
+  const { data, isLoading } = usePurchaseFrequency({
+    from: startDate,
+    to: endDate,
+  });
 
   const handleInput = (type: 'start' | 'end') => {
     setIsOpenCalendar(true);
@@ -65,7 +59,11 @@ const PurchaseFrequencySection = () => {
         </S.DateRangeContainer>
       </S.Header>
 
-      <BarChart data={sampleData} xAxisKey="range" barKey="count" formatXAxisLabel={formatRange} />
+      {isLoading ? (
+        <div>로딩 중...</div>
+      ) : (
+        <BarChart data={data} xAxisKey="range" barKey="count" formatXAxisLabel={formatRange} />
+      )}
     </S.Container>
   );
 };
