be31a384b3c97fa79307fb233e4052867bc6c36a
suhwa lee <tnghk9611@naver.com>
2025-12-23 18:26:31 +0900

    feat: ModalProvider 감싸기

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { ModalProvider } from '@/shared/components/Modal/ModalContext';
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  return <Outlet />;
+  return (
+    <ModalProvider>
+      <Outlet />
+    </ModalProvider>
+  );
 };
 
 export default App;
