894630de4b98777e0775e04b17afa0265b213c7b
suhwa lee <tnghk9611@naver.com>
2025-12-23 10:52:25 +0900

    style: main에 스타일 적용

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import './styles/global.css';
+import './styles/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
